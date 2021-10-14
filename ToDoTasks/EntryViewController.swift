c11232ad63d5992714013adf5ee2c2fc846a56c0
rawezh <appsazubi@mac-s0126764.fritz.box>
2021-10-14 21:57:38 +0200

    new development

--- ToDoTasks/EntryViewController.swift
+++ ToDoTasks/EntryViewController.swift
@@ -11,6 +11,8 @@ class EntryViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var field: UITextField!
 
+    var update: (() -> Void)?
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,9 +31,20 @@ class EntryViewController: UIViewController, UITextFieldDelegate {
             return
         }
 
-        var count = UserDefaults().value(forKey: "count") as? Int
+        guard let count = UserDefaults().value(forKey: "count") as? Int else {
+            return
+        }
+
+        let newCount = count + 1
+
+        UserDefaults().set(newCount, forKey: "count")
+
+        UserDefaults().set(text, forKey: "task_\(newCount)")
 
 
+        update?()
+
+        navigationController?.popViewController(animated: true)
     }
 
 }
